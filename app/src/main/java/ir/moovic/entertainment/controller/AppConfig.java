866ec0e01ebff7534f5f52b45507acfc84b68fe3
erfand <dadras.erfan@yahoo.com>
2019-11-11 18:59:54 +0330

    is a test

--- app/src/main/java/ir/moovic/entertainment/controller/AppConfig.java
+++ app/src/main/java/ir/moovic/entertainment/controller/AppConfig.java
@@ -5,6 +5,10 @@ import android.text.TextUtils;
 
 public class AppConfig {
 
+
+    //////////erfan
+
+
     public static final String BASE_URL = "http://avic.moovic.ir/";
     public static final String MEDIA_BASE_URL = BASE_URL;
 
